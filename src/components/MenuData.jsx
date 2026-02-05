f2d4fac7fc3aa2b979c47329b43823f4f76b6dde
ATHIPATLA-JASWITHA <jaswitha.23BCB7267@vitapstudent.ac.in>
2026-02-05 12:45:16 +0530

    OurMess

--- src/components/MenuData.jsx
+++ src/components/MenuData.jsx
@@ -1,44 +1,231 @@
-export const weeklyMenu = {
-  Monday: {
-    breakfast: "Chole Bhature, Veg Pongal, Chutney, Bread, Milk",
-    lunch: "White Rice, Phulka, Ladies Finger Poriyal, Muddha Pappu",
-    snacks: "Masala Sweet Corn / Dry Maggi",
-    dinner: "Roti, Pudina Rice, Chilli Paneer / Chicken Fry, Dal Tadka"
+export const februaryMenu = {
+  "Feb 1": {
+    day: "Sunday",
+    breakfast: "Aloo Paratha, Chutney, Bread, Milk",
+    lunch: "Chicken Dum Biryani / Paneer Dum Biryani, Raita",
+    snacks: "Pani Poori",
+    dinner: "Phulka, Set Dosa, Veg Curry, Soup"
   },
-  Tuesday: {
-    breakfast: "Masala Dosa, Vada Pav, Chutney, Milk",
-    lunch: "Chapathi, Bisibele Bath, Rajma Masala",
-    snacks: "Aloo Samosa / Punugulu",
-    dinner: "Phulka, Veg Noodles, Veg Kurma / Egg Bhurji"
+
+  "Feb 2": {
+    day: "Monday",
+    breakfast: "Chole Bhature, Pongal, Chutney, Bread, Milk, Boiled Egg",
+    lunch: "White Rice, Phulka, Muddha Pappu, Veg Curry, Rasam, Boondi Laddu",
+    snacks: "Stuffed Veg Roll, Tea/Coffee",
+    dinner: "Chapathi, Kadai Paneer / Mughlai Chicken, Dal Tadka, Soup"
   },
-  Wednesday: {
-    breakfast: "Pongal, Medu Vada, Coconut Chutney",
-    lunch: "Bhagara Rice, Chicken Curry / Paneer Butter Masala",
-    snacks: "Mirchi Bhajji",
-    dinner: "Chapathi, Carrot Rice, Tomato Dal"
+
+  "Feb 3": {
+    day: "Tuesday",
+    breakfast: "Idli, Vada, Chutney, Bread, Milk, Cornflakes, Banana",
+    lunch: "White Rice, Roti, Rajma Masala, Tomato Chutney, Doodh Peda",
+    snacks: "Samosa / Imli Chutney",
+    dinner: "Veg Biryani / Mushroom Biryani, Veg Kurma, Rasam, Soup"
   },
-  Thursday: {
-    breakfast: "Poori, Semiya Upma, Chutney",
-    lunch: "Lemon Rice, Veg Do Pyaza",
+
+  "Feb 4": {
+    day: "Wednesday",
+    breakfast: "Masala Dosa, Pav Bhaji, Chutney, Milk, Egg",
+    lunch: "Paneer Manchurian / Chicken 65, White Rice, Dal, Rasam, Rasmalai",
+    snacks: "Masala Sweet Corn",
+    dinner: "Chapathi, Veg Noodles, Dal Fry, Soup"
+  },
+
+  "Feb 5": {
+    day: "Thursday",
+    breakfast: "Poori, Potato Masala, Bread, Milk, Apple Juice",
+    lunch: "White Rice, Phulka, Veg Pappu, Curd, Shahi Tukda",
+    snacks: "Banana Bajji",
+    dinner: "Roti, Veg Rice, Veg Curry, Soup"
+  },
+
+  "Feb 6": {
+    day: "Friday",
+    breakfast: "Onion Uttapam, Coconut Chutney, Bread, Milk, Grape Juice",
+    lunch: "White Rice, Dal Makhani, Drumstick Curry, Rava Kesari",
+    snacks: "Boiled Peanut Masala",
+    dinner: "Lacha Paratha, Butter Chicken / Paneer Butter Masala, Soup"
+  },
+
+  "Feb 7": {
+    day: "Saturday",
+    breakfast: "Mysore Bonda, Idly, Chutney, Milk",
+    lunch: "White Rice, Tomato Dal, Veg Fry, Papad, Gulab Jamun",
+    snacks: "Bread Pakoda",
+    dinner: "Dal Punjabi, Veg Fried Rice, Veg Manchurian, Soup"
+  },
+
+  // ===== WEEK 2 =====
+
+  "Feb 8": {
+    day: "Sunday",
+    breakfast: "Aloo Paratha, Chutney, Bread, Milk",
+    lunch: "Chicken Dum Biryani / Paneer Dum Biryani, Raita",
+    snacks: "Pani Poori",
+    dinner: "Phulka, Set Dosa, Veg Curry, Soup"
+  },
+
+  "Feb 9": {
+    day: "Monday",
+    breakfast: "Chole Bhature, Pongal, Chutney, Bread, Milk",
+    lunch: "White Rice, Phulka, Kadai Veg, Rasam, Laddu",
+    snacks: "Veg Roll",
+    dinner: "Chapathi, Paneer Butter Masala / Chicken Curry, Dal Tadka"
+  },
+
+  "Feb 10": {
+    day: "Tuesday",
+    breakfast: "Masala Dosa, Upma, Chutney, Milk",
+    lunch: "White Rice, Rajma, Veg Fry, Semiya Payasam",
+    snacks: "Punugulu",
+    dinner: "Veg Biryani / Egg Curry, Soup"
+  },
+
+  "Feb 11": {
+    day: "Wednesday",
+    breakfast: "Idli, Vada, Chutney, Milk",
+    lunch: "Paneer 65 / Andhra Chicken Fry, Rice, Dal, Pongal",
+    snacks: "Sweet Corn",
+    dinner: "Chapathi, Palakura Pappu, Aloo Fry, Soup"
+  },
+
+  "Feb 12": {
+    day: "Thursday",
+    breakfast: "Poori, Potato Bhaji, Milk",
+    lunch: "Veg Pulav, Dal, Mango Dal",
+    snacks: "Coleslaw Sandwich",
+    dinner: "Veg Pasta, Soya Curry, Soup"
+  },
+
+  "Feb 13": {
+    day: "Friday",
+    breakfast: "Mysore Bonda, Semiya Khichdi",
+    lunch: "Tomato Rice, Cabbage Kootu, Sweet Halwa",
     snacks: "Papdi Chat",
-    dinner: "Roti, Schezwan Fried Rice, Dal Punjabi"
+    dinner: "Chicken Chettinad / Kadai Paneer, Soup"
   },
-  Friday: {
-    breakfast: "Mysore Bonda, Tomato Upma",
-    lunch: "Mix Veg Pulav, Dal Maharani",
-    snacks: "Sambar Vada",
-    dinner: "Lacha Paratha, Chettinad Chicken / Kadai Paneer"
+
+  "Feb 14": {
+    day: "Saturday",
+    breakfast: "Plain Dosa, Pav Bhaji",
+    lunch: "Veg Pulav, Dal Tadka, Bread Halwa",
+    snacks: "Onion Pakoda",
+    dinner: "Veg Kofta, Tomato Dal, Soup"
   },
-  Saturday: {
-    breakfast: "Uttappam, Uggani",
-    lunch: "Tomato Rice, Aloo Fry",
-    snacks: "Onion Samosa",
-    dinner: "Veg Pasta, Dal Maharani"
-  },
-  Sunday: {
-    breakfast: "Aloo Paratha, Wheat Upma",
+
+  // ===== WEEK 3 =====
+
+  "Feb 15": {
+    day: "Sunday",
+    breakfast: "Aloo Paratha, Chutney, Milk",
     lunch: "Chicken Dum Biryani / Paneer Biryani",
     snacks: "Pani Poori",
-    dinner: "Roti, Dal Makhani, Set Dosa"
+    dinner: "Idly, Jeera Dal, Soup"
+  },
+
+  "Feb 16": {
+    day: "Monday",
+    breakfast: "Chole Bhature, Pongal",
+    lunch: "Rice, Phulka, Veg Curry, Rasam",
+    snacks: "Veg Roll",
+    dinner: "Chapathi, Kadai Paneer / Chicken"
+  },
+
+  "Feb 17": {
+    day: "Tuesday",
+    breakfast: "Idli, Vada",
+    lunch: "Rice, Rajma, Sweet",
+    snacks: "Samosa",
+    dinner: "Veg Kurma, Soup"
+  },
+
+  "Feb 18": {
+    day: "Wednesday",
+    breakfast: "Masala Dosa",
+    lunch: "Paneer 65 / Chicken 65",
+    snacks: "Sweet Corn",
+    dinner: "Veg Noodles"
+  },
+
+  "Feb 19": {
+    day: "Thursday",
+    breakfast: "Poori",
+    lunch: "Veg Pappu",
+    snacks: "Banana Bajji",
+    dinner: "Veg Rice"
+  },
+
+  "Feb 20": {
+    day: "Friday",
+    breakfast: "Uttapam",
+    lunch: "Dal Makhani",
+    snacks: "Peanut Masala",
+    dinner: "Butter Chicken / Paneer"
+  },
+
+  "Feb 21": {
+    day: "Saturday",
+    breakfast: "Bonda",
+    lunch: "Tomato Dal",
+    snacks: "Pakoda",
+    dinner: "Veg Fried Rice"
+  },
+
+  // ===== WEEK 4 =====
+
+  "Feb 22": {
+    day: "Sunday",
+    breakfast: "Paratha",
+    lunch: "Biryani",
+    snacks: "Pani Poori",
+    dinner: "Set Dosa"
+  },
+
+  "Feb 23": {
+    day: "Monday",
+    breakfast: "Chole Bhature",
+    lunch: "Veg Meals",
+    snacks: "Veg Roll",
+    dinner: "Chapathi"
+  },
+
+  "Feb 24": {
+    day: "Tuesday",
+    breakfast: "Masala Dosa",
+    lunch: "Rajma",
+    snacks: "Punugulu",
+    dinner: "Veg Kurma"
+  },
+
+  "Feb 25": {
+    day: "Wednesday",
+    breakfast: "Idli",
+    lunch: "Paneer / Chicken",
+    snacks: "Sweet Corn",
+    dinner: "Dal Fry"
+  },
+
+  "Feb 26": {
+    day: "Thursday",
+    breakfast: "Poori",
+    lunch: "Veg Pulav",
+    snacks: "Sandwich",
+    dinner: "Veg Pasta"
+  },
+
+  "Feb 27": {
+    day: "Friday",
+    breakfast: "Bonda",
+    lunch: "Tomato Rice",
+    snacks: "Chat",
+    dinner: "Chicken / Paneer"
+  },
+
+  "Feb 28": {
+    day: "Saturday",
+    breakfast: "Dosa",
+    lunch: "Veg Pulav",
+    snacks: "Pakoda",
+    dinner: "Veg Curry"
   }
 };
